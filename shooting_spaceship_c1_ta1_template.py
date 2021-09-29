739f21c400bbd5da3630476d8f4791de83b5ec50
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-09-29 20:13:49 +0530

    Create shooting_spaceship_c1_ta1_template.py

---
+++ shooting_spaceship_c1_ta1_template.py
@@ -0,0 +1,26 @@
+# Importing 'pygame' 
+
+
+# Initialising 'pygame' 
+
+# Creating game window of width 400 and height 600
+
+
+# Setting the title of the game
+
+
+# Loading the background image
+
+# Displaying the 'background_image' at the location [0,0]
+
+
+# Creating BLUE color using RGB combinations and naming it as 'BLUE'
+
+
+# Creating a rectangle at the coordinates of 200,200 with width and height as 30
+# Name the rectangle as 'player'
+
+# Drawing the 'player' rectangle in the screen in BLUE color
+
+
+# Updating the screen after adding the background image and drawing the rectangle
